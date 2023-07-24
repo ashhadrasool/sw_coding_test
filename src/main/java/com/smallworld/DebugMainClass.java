c30cbd6b727bb2722255d42f38c4e16de7e289e9
Ashhad Rasool <ashhad.rasool22@gmail.com>
2023-07-24 22:30:48 +0500

    using bean of TransactionDataFetcher

--- src/main/java/com/smallworld/DebugMainClass.java
+++ src/main/java/com/smallworld/DebugMainClass.java
@@ -2,14 +2,19 @@ package com.smallworld;
 
 import com.smallworld.config.AppConfig;
 import com.smallworld.repository.TransactionRepository;
+import com.smallworld.service.TransactionDataFetcher;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class DebugMainClass {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
         TransactionRepository transactionRepository = context.getBean(TransactionRepository.class);
         System.out.println(transactionRepository.findAll().size());
+
+        TransactionDataFetcher transactionDataFetcher = context.getBean(TransactionDataFetcher.class);
+        System.out.println("Top Sender: "+ transactionDataFetcher.getTopSender().orElse(""));
     }
 
 }
--- src/main/java/com/smallworld/config/AppConfig.java
+++ src/main/java/com/smallworld/config/AppConfig.java
@@ -3,6 +3,8 @@ package com.smallworld.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.smallworld.repository.TransactionRepository;
 import com.smallworld.repository.impl.TransactionJsonFileRepositoryImpl;
+import com.smallworld.service.TransactionDataFetcher;
+import com.smallworld.service.impl.TransactionDataFetcherImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +21,11 @@ public class AppConfig {
     @Bean("transactionRepository")
     TransactionRepository getTransactionRepository(ObjectMapper objectMapper){
         return new TransactionJsonFileRepositoryImpl(objectMapper);
+    }
+
+    @Bean("transactionDataFetcher")
+    TransactionDataFetcher getTransactionRepository(TransactionRepository transactionRepository){
+        return new TransactionDataFetcherImpl(transactionRepository);
     }
 
 }
